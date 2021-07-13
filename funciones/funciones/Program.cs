8eaec63fa667f7d68fde732167e9113fc6a129c1
Josue Garcia <josuemauriciog@hotmail.com>
2021-07-12 21:36:49 -0600

    Agregando ejercicios POO

---
+++ funciones/funciones/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Jorge", "Alvarez");
+            Alumno b = new Alumno(2, "Hector", "Acosta");
+
+            a.inactivarAlumno();              
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine("");
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+        }
+    }
+}
---
+++ propiedades/constructores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Marta";
+            b.SegundoNombre = "Diaz";
+
+            Alumno c = new Alumno(3);
+            b.PrimerNombre = "Pedro";
+
+            Alumno d = new Alumno("Raul", "Jimenez");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+        }
+    }
+}
